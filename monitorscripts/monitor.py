a3f3431d4d9e8a1635dc221f9935939486e6afc7
Tally Wiesenberg <tallywiesenberg@Tallys-MacBook-Air.local>
2021-09-15 10:59:40 -0400

    first pass mvp

--- monitorscripts/monitor.py
+++ monitorscripts/monitor.py
@@ -1,10 +1,14 @@
 import pandas as pd
+import streamlit as st
+
+from tellor_api_utils import TellorAPIUtils
 
 class Monitor:
 
-    def __init__(self, networks, request_ids) -> None:
+    def __init__(self) -> None:
         self.networks = []
         self.request_ids = []
+        self.tau = TellorAPIUtils()
 
         self.status_df = pd.DataFrame()
         
@@ -14,7 +18,21 @@ class Monitor:
 
     def make_monitor_dataframe(self):
         '''Creates monitor for display using streamlit'''
+        
+        list_of_dicts = []
 
-
+        for i in self.request_ids:
+            #add request Id status to df
+            request_id_dict = self.tau.get_request_id_status(i)
+            list_of_dicts.append(request_id_dict)
+        
+        self.status_df = pd.DataFrame(list_of_dicts)
+        
+        #apply coloring
+        self.status_df.style.applymap(
+            lambda x: "background-color: red" if x == "red" else ("backgrond-color: yellow" if x == "yellow" else "background-color: white")
+        )
+        
         #display streamlit table
-    
+        st.dataframe(self.status_df)
+            
--- monitorscripts/run.py
+++ monitorscripts/run.py
@@ -1,5 +1,10 @@
 from monitor import Monitor
 
-
+current_monitored_ids = [1, 2, 10]
 
 m = Monitor()
+
+for i in current_monitored_ids:
+    m.add_request_id(i)
+
+m.make_monitor_dataframe()
