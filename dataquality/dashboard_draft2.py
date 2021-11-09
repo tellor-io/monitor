711d89abfc0ea01e3684f48099b8b77ecd867073
lauren <lmg2@illinois.edu>
2021-11-09 16:12:07 -0600

    secondary price feeds added
    at least one additional price feed per plot added, as well as  enhanced plotting formatting, beginning of datatable health addition, and data filtering for ampleforth

--- dataquality/dashboard_draft2.py
+++ dataquality/dashboard_draft2.py
@@ -1,8 +1,10 @@
 import dash
 from dash import dcc
 from dash import html
+import dash_table
 import plotly.express as px
 import pandas as pd
+import plotly.graph_objects as go
 
 
 app = dash.Dash(__name__)
@@ -15,20 +17,22 @@ from datetime import datetime, timedelta
 import pandas as pd
 import numpy as np
 from dateutil import parser
+import time
+import requests
 
 #connect to database
-con = sqlite3.connect('tellor_dashboard_v0.db')
+con = sqlite3.connect('tellor_dashboard_v1.db')
 c = con.cursor()
 
-df = pd.read_sql("SELECT * FROM tellor_table_2", con)
+df = pd.read_sql("SELECT * FROM tellor_table_7", con)
 
 df2 = df.sort_values(by="timestamp")
 
 ###################################
-### TELLOR DATA SECTION (1)
+###  DATA QUALITY SECTION (1)
 ###################################
 
-################################### FIGURE 0: ETH/USD
+################################### FIGURE 0: ETH/USD (ID:1)
 fig = px.line(df2.loc[df2.id == 1], x="timestamp", y="price",
                   color = 'category' , template = 'plotly_dark', title = 'ETH/USD',
                   color_discrete_sequence = ['aquamarine', 'tomato'])
@@ -40,23 +44,20 @@ fig.update_traces(hovertemplate = 'Price: %{y:$.2f}<extra></extra>')
 fig.update_traces(line=dict(width=1))
 fig.update_layout({'legend_title_text': ''})
 
-################################### FIGURE 01: BTC/USD
+################################### FIGURE 1: BTC/USD (ID:2)
+
 fig1 = px.line(df2.loc[df2.id == 2], x="timestamp", y="price",
-                  color="category", template = 'plotly_dark', title = 'BTC/USD')
+                  color="category", template = 'plotly_dark', title = 'BTC/USD', color_discrete_sequence = ['aquamarine','tomato'])
 fig1.update_layout(xaxis_title = 'date', title_x = 0.5)
 fig1.update_layout(hovermode="x")
 fig1.update_traces(hovertemplate = 'Price: %{y:$.2f}<extra></extra>')
 #fig1.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)','paper_bgcolor': 'rgba(0, 0, 0, 0)'})
-fig1.update_traces(line=dict(color="aquamarine", width=1))
+fig1.update_traces(line=dict(width=1))
 fig1.update_layout({'legend_title_text': ''})
 
-################################### FIGURE 0: AMPL/USD
+################################### FIGURE 2: AMPL/USD (ID:10)
 
-
-
-
-#fig2 = px.line(df4, x="timestamp", y="price",template = 'plotly_dark', title = 'AMPL/USD price via Tellor')
-fig2 = px.line(df2.loc[df2.id == 10], x="timestamp", y="price",template = 'plotly_dark', title = 'AMPL/USD', color = 'category', color_discrete_sequence = ['tomato','aquamarine', 'mediumslateblue'])
+fig2 = px.line(df2.loc[df2.id == 10], x="timestamp", y="price",template = 'plotly_dark', title = 'AMPL/USD', color = 'category', color_discrete_sequence = ['tomato', 'mediumslateblue','aquamarine'])
 fig2.update_layout(xaxis_title = 'date', title_x = 0.5)
 fig2.update_layout(hovermode="x")
 fig2.update_traces(hovertemplate = 'Price: %{y:$.2f}<extra></extra>')
@@ -64,6 +65,71 @@ fig2.update_traces(hovertemplate = 'Price: %{y:$.2f}<extra></extra>')
 #fig2.update_traces(line=dict(color="aquamarine", width=1))
 fig2.update_layout({'legend_title_text': ''})
 fig2.update_traces(line=dict(width=1))
+
+################################### FIGURE 3: ETH/BTC (ID:5)
+fig3 = px.line(df2.loc[df2.id == 5], x="timestamp", y="price",template = 'plotly_dark', title = 'ETH/BTC', color = 'category', color_discrete_sequence = ['tomato','aquamarine', 'mediumslateblue'])
+fig3.update_layout(xaxis_title = 'date', title_x = 0.5)
+fig3.update_layout(hovermode="x")
+fig3.update_traces(hovertemplate = '%{y:.4f}<extra></extra>')
+#fig2.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)','paper_bgcolor': 'rgba(0, 0, 0, 0)'})
+#fig2.update_traces(line=dict(color="aquamarine", width=1))
+fig3.update_layout({'legend_title_text': ''})
+fig3.update_traces(line=dict(width=1))
+
+################################### FIGURE 4: DEFIVTL (ID:57)
+fig4 = px.line(df2.loc[df2.id == 57], x="timestamp", y="price",template = 'plotly_dark', title = 'DEFITVL (USD)', color = 'category', color_discrete_sequence = ['aquamarine', 'fuchsia'])
+fig4.update_layout(xaxis_title = 'date', title_x = 0.5)
+fig4.update_layout(hovermode="x")
+fig4.update_traces(hovertemplate = 'Price: %{y:$.2f}<extra></extra>')
+#fig2.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)','paper_bgcolor': 'rgba(0, 0, 0, 0)'})
+#fig2.update_traces(line=dict(color="aquamarine", width=1))
+fig4.update_layout({'legend_title_text': ''})
+fig4.update_traces(line=dict(width=1))
+
+################################### FIGURE 5: ETH/JPY (ID:59)
+### IGNORE FOR NOW
+
+
+### TABLE FOR UPDATE MONITORING
+tellor_api = "http://api.tellorscan.com/prices/1"
+time_gap = {"yellow": 12*60*60, "red": 24*60*60}
+r = requests.get(tellor_api)
+files = r.json()
+df_list = []
+for file in files:
+    time_update = int(file['timestamp'])
+    diff = round((time.time() - time_update) / 3600 , 3)
+    if diff >= 24:
+        flag = 'red'
+    elif diff >= 12:
+        flag = 'yellow'
+    else:
+        flag = 'green'
+        
+    df_list.append([file['id'], file['name'], diff, flag])
+    
+
+df_tab = pd.DataFrame(df_list, columns = ['id', 'price feed', 'hours since last update', 'status'])
+
+
+
+color_list = ['mediumspringgreen' if v <= 12 else 'tomato' if v >= 24 else 'yellow' for v in df_tab['hours since last update']]
+
+
+figt = go.Figure(data = [go.Table(
+  header = dict(
+    values = ['id', 'price feed', 'hours since last update'], align = 'center'),
+  cells = dict(values = [df_tab['id'], df_tab['price feed'], df_tab['hours since last update']], 
+  fill_color = ['rgba(0,0,0,0)', 'rgba(0,0,0,0)', color_list], font_color = ['darkslategray', 'darkslategray', 'white'])
+
+  )
+])
+figt.update_layout(title_text = 'Tellor Price Feed Health', title_x = 0.5)
+figt.update_layout(autosize=False,
+                  width=500,
+                  height=750
+                 )
+
 
 
 
@@ -75,7 +141,8 @@ app.layout = html.Div(children=[
                                   children = [
                                     html.H2('TELLOR DASHBOARD'),
                                     html.P('''Visualising Tellor data with Plotly - Dash'''),
-                                    html.P('''Pick a visualization option from the dropdown below.''') 
+                                    
+                                    dcc.Graph( figure = figt) 
 
                                 ]),  # Define the left element
                                   html.Div(className='eight columns div-for-charts bg-grey',
@@ -91,6 +158,14 @@ app.layout = html.Div(children=[
                                     dcc.Graph(
                                     id='tellor AMPL/USD',
                                     figure=fig2
+                                ),
+                                    dcc.Graph(
+                                    id='tellor ETH/BTC',
+                                    figure=fig3
+                                ),
+                                    dcc.Graph(
+                                    id='tellor DEFITVL',
+                                    figure=fig4
                                 )
                                   ])  
                                   ])
