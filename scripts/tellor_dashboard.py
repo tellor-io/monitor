7ea1dfd46d91b2a982ba8db131ce1285111e6328
lauren gregory <lmg2@illinois.edu>
2021-12-31 11:28:21 -0500

    cosmetic changes

--- scripts/tellor_dashboard.py
+++ scripts/tellor_dashboard.py
@@ -2,21 +2,16 @@ import dash
 from dash import dcc
 from dash import html
 from dash import dash_table
+from datetime import datetime
 import plotly.express as px
-import sqlite3
 import pandas as pd
 from sqlalchemy import create_engine
 import os
 from dotenv import load_dotenv
 
-
-
 app = dash.Dash(__name__)
 
-# connect to database
 load_dotenv('../.env')
-#con = sqlite3.connect('../data/tellor.db')
-#c = con.cursor()
 
 engine_string = "postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}".format(
     user = os.getenv('DB_USER'),
@@ -27,13 +22,8 @@ engine_string = "postgresql+psycopg2://{user}:{password}@{host}:{port}/{database
 )
 
 engine = create_engine(engine_string)
-
-# read a table from database into pandas dataframe, replace "tablename" with your table name
 df = pd.read_sql_table('test',engine)
-
-#df = pd.read_sql("SELECT * FROM tellor_datatable", con)
 df2 = df.sort_values(by = "time")
-
 
 ###################################
 ###  DATA QUALITY SECTION (1)
@@ -41,41 +31,118 @@ df2 = df.sort_values(by = "time")
 
 ################################### FIGURE 0: ETH/USD (ID:1)
 fig = px.line(df2.loc[df2.id == 1], x="time", y="price",
-              color='oracle', template='plotly_dark', title='ETH/USD',
-              color_discrete_sequence=['aquamarine', 'tomato'])
+              color='oracle', template='plotly', title='ETH/USD',
+              color_discrete_sequence=['#3fd491', '#233047'])
 
 fig.update_layout(xaxis_title='date', title_x=0.5)
 fig.update_layout(hovermode="x")
 fig.update_traces(hovertemplate='Price: %{y:$.2f}<extra></extra>')
-# fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)','paper_bgcolor': 'rgba(0, 0, 0, 0)'})
-fig.update_traces(line=dict(width=1))
 fig.update_layout({'legend_title_text': ''})
+fig.update_layout(font_family="Inconsolata")
+fig.update_layout(font_color='#535959')
+fig.update_traces(line=dict(width=2))
+fig.update_layout(
+    xaxis=dict(
+        rangeselector=dict(
+            buttons=list([
+                dict(count=1,
+                     label="1d",
+                     step="day",
+                     stepmode="backward"),
+                dict(count=7,
+                     label="1w",
+                     step="day",
+                     stepmode="backward"),
+                dict(count=1,
+                     label="1m",
+                     step="month",
+                     stepmode="backward"),
+                dict(step="all")
+            ])
+        ),
+        rangeslider=dict(
+            visible=False
+        ),
+        type="date"
+    )
+)
 
 ################################### FIGURE 1: BTC/USD (ID:2)
 
 fig1 = px.line(df2.loc[df2.id == 2], x="time", y="price",
-               color="oracle", template='plotly_dark', title='BTC/USD',
-               color_discrete_sequence=['aquamarine', 'tomato'])
+               color="oracle", template='plotly', title='BTC/USD',
+               color_discrete_sequence=['#3fd491', '#233047'])
 fig1.update_layout(xaxis_title='date', title_x=0.5)
 fig1.update_layout(hovermode="x")
 fig1.update_traces(hovertemplate='Price: %{y:$.2f}<extra></extra>')
 # fig1.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)','paper_bgcolor': 'rgba(0, 0, 0, 0)'})
-fig1.update_traces(line=dict(width=1))
+fig1.update_traces(line=dict(width=2))
 fig1.update_layout({'legend_title_text': ''})
+fig1.update_layout(font_family="Inconsolata")
+fig1.update_layout(font_color='#535959')
+fig1.update_layout(
+    xaxis=dict(
+        rangeselector=dict(
+            buttons=list([
+                dict(count=1,
+                     label="1d",
+                     step="day",
+                     stepmode="backward"),
+                dict(count=7,
+                     label="1w",
+                     step="day",
+                     stepmode="backward"),
+                dict(count=1,
+                     label="1m",
+                     step="month",
+                     stepmode="backward"),
+                dict(step="all")
+            ])
+        ),
+        rangeslider=dict(
+            visible=False
+        ),
+        type="date"
+    )
+)
 
 ################################### FIGURE 2: AMPL/USD
 # fig2 = px.line(df4, x="timestamp", y="price",template = 'plotly_dark', title = 'AMPL/USD price via Tellor')
-fig2 = px.line(df2.loc[df2.id == 10], x="time", y="price", template='plotly_dark', title='AMPL/USD',
-               color='oracle', color_discrete_sequence=['aquamarine', 'tomato', 'mediumslateblue'])
+fig2 = px.line(df2.loc[df2.id == 10], x="time", y="price", template='plotly', title='AMPL/USD',
+               color='oracle', color_discrete_sequence=['#3fd491', '#233047', 'mediumslateblue'])
 
 fig2.update_layout(xaxis_title='date', title_x=0.5)
 fig2.update_layout(hovermode="x")
 fig2.update_traces(hovertemplate='Price: %{y:$.2f}<extra></extra>')
-# fig2.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)','paper_bgcolor': 'rgba(0, 0, 0, 0)'})
-# fig2.update_traces(line=dict(color="aquamarine", width=1))
 fig2.update_layout({'legend_title_text': ''})
-fig2.update_traces(line=dict(width=1))
-
+fig2.update_traces(line=dict(width=2))
+fig2.update_layout(font_color='#535959')
+fig2.update_layout(font_family="Inconsolata")
+fig2.update_layout(
+    xaxis=dict(
+        rangeselector=dict(
+            buttons=list([
+                dict(count=1,
+                     label="1d",
+                     step="day",
+                     stepmode="backward"),
+                dict(count=7,
+                     label="1w",
+                     step="day",
+                     stepmode="backward"),
+                dict(count=1,
+                     label="1m",
+                     step="month",
+                     stepmode="backward"),
+                dict(step="all")
+            ])
+        ),
+        rangeslider=dict(
+            visible=False
+        ),
+        type="date"
+    )
+)
 
 '''
 ### TABLE FOR UPDATE MONITORING
@@ -94,14 +161,28 @@ for file in files:
 
 df_tab = pd.DataFrame(df_list, columns=['id', 'price feed', 'hours since last update'])
 '''
+
+important_ids = [1, 2, 10]
+#timestamp, price, id, oracle)
+table = []
+df_tel = df2[df2['oracle'] == 'tellor']
+
+for id in important_ids:
+    df_sub = df_tel[df_tel['id']== id]['timestamp'].max()
+    change = (datetime.now() - datetime.fromisoformat(df_sub)).total_seconds() / 3600
+    table.append([id, change])
+
+df_tab = pd.DataFrame(table, columns = ['id', 'hours since last update'])
+
+
 app.layout = html.Div(children=[
     html.Div(className='row',  # Define the row element
              children=[
                  html.Div(className='four columns div-user-controls',
                           children=[
-                              html.H2('TELLOR DASHBOARD'),
-                              html.P('''Visualising Tellor data with Plotly - Dash'''),
-                            '''
+                              html.H2('tellor dashboard'),
+                              html.P('''visualising tellor data with plotly - dash'''),
+
                               dash_table.DataTable(
                                   id='table_id',
                                   columns=[{'name': i, 'id': i} for i in df_tab.columns],
@@ -113,23 +194,23 @@ app.layout = html.Div(children=[
                                           'filter_query': '{hours since last update} >= 12 && {hours since last update} < 24',
                                           'column_id': 'hours since last update'
                                           },
-                                       'backgroundColor': 'yellow',
+                                       'backgroundColor': '#5a5c5c',
                                        'color': 'white'
                                        },
                                       {'if': {'filter_query': '{hours since last update} < 12',
                                               'column_id': 'hours since last update'
                                               },
-                                       'backgroundColor': 'mediumspringgreen',
+                                       'backgroundColor': '#3fd491',
                                        'color': 'white'
                                        },
                                       {'if': {'filter_query': '{hours since last update} >= 24',
                                               'column_id': 'hours since last update'
                                               },
-                                       'backgroundColor': 'tomato',
+                                       'backgroundColor': '#1a1a1a',
                                        'color': 'white'
                                        }
                                   ]
-                              ) '''
+                              )
 
                           ]),  # Define the left element
                  html.Div(className='eight columns div-for-charts bg-grey',
